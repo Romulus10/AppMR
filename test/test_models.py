9f31c90a4e3728dc6c59481cd2cce4343c796e16
Sean Batzel <romulus108@protonmail.com>
2021-10-26 13:31:02 -0400

    Clearing out print statements.

--- test/test_models.py
+++ test/test_models.py
@@ -1,9 +1,6 @@
-from pprint import pprint
-
 from model_bakery import baker
 
 
 def test_support_ticket():
     st = baker.make('appMR.SupportTicket')
-    pprint(st)
     assert str(st) == st.title
