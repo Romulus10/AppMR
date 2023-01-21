2a4a99bfa65f3a058f9c6e558ca50d4fe2f4dce9
Sean Batzel <romulus108@protonmail.com>
2023-01-21 16:14:59 -0500

    Updated URL confs for Django 4+

--- urls.py
+++ urls.py
@@ -1,8 +1,7 @@
 """
 app_mr URL configurations.
 """
-from django.conf.urls import url
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import (
     not_logged_in,
@@ -17,13 +16,13 @@ from .views import (
 app_name = "app_mr"
 
 urlpatterns = [
-    url(r"^$", bug_list_view, name="index"),
+    re_path(r"^$", bug_list_view, name="index"),
     path(r"list/<int:ticket_type>", bug_list_view, name="bug_list_view"),
-    url(r"^new/$", new_bug_view, name="new_bug_view"),
+    re_path(r"^new/$", new_bug_view, name="new_bug_view"),
     path(r"detail/<int:ticket_id>", bug_detail_view, name="bug_detail_view"),
     path(r"comment/<int:bug_id>", post_comment_view, name="post_comment_view"),
     path(
         r"change/<int:ticket_type>", change_bug_list_view, name="change_bug_list_view"
     ),
-    url(r"^not_logged_in/$", not_logged_in, name="not_logged_in"),
+    re_path(r"^not_logged_in/$", not_logged_in, name="not_logged_in"),
 ]
